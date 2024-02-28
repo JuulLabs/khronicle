7040636546ca21e011b6b8f9f51a765e5b663e15
Travis Wyatt <travis@juul.com>
2024-02-28 10:53:58 -0800

    Simplify Gradle configuration (#3)

--- khronicle-core/build.gradle.kts
+++ khronicle-core/build.gradle.kts
@@ -11,39 +11,21 @@ kotlin {
     explicitApi()
     jvmToolchain(libs.versions.jvm.toolchain.get().toInt())
 
-    jvm()
-    js().browser()
-    macosX64()
-    macosArm64()
-    iosX64()
     iosArm64()
     iosSimulatorArm64()
+    iosX64()
+    js().browser()
+    jvm()
+    macosArm64()
+    macosX64()
 
     sourceSets {
-
         all {
             languageSettings.optIn("com.juul.khronicle.KhronicleInternal")
         }
 
-        val commonMain by getting
-
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-                implementation(kotlin("test-annotations-common"))
-            }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
         }
-
-//        val jvmTest by getting {
-//            dependencies {
-//                implementation(kotlin("test-junit"))
-//            }
-//        }
-//
-//        val jsTest by getting {
-//            dependencies {
-//                implementation(kotlin("test-js"))
-//            }
-//        }
     }
 }
