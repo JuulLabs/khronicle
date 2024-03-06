87d73ec0b77f7d39cc1478a1aa3610f136a02089
Travis Wyatt <travis@juul.com>
2024-03-06 11:00:22 -0800

    Simplify `binary-compatibility-validator` setup (#18)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,15 +8,8 @@ pluginManagement {
 
     resolutionStrategy {
         eachPlugin {
-            when (requested.id.id) {
-                "binary-compatibility-validator" ->
-                    useModule("org.jetbrains.kotlinx:binary-compatibility-validator:${requested.version}")
-
-                else -> when (requested.id.namespace) {
-                    "com.android" ->
-                        useModule("com.android.tools.build:gradle:${requested.version}")
-                }
-            }
+            if (requested.id.namespace == "com.android")
+                useModule("com.android.tools.build:gradle:${requested.version}")
         }
     }
 }
