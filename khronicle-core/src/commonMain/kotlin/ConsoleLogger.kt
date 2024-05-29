0b3206147fe76ebe368f38c6a17054e897f20156
Travis Wyatt <travis@juul.com>
2024-05-29 14:53:59 -0700

    Kotlinter

--- khronicle-core/src/commonMain/kotlin/ConsoleLogger.kt
+++ khronicle-core/src/commonMain/kotlin/ConsoleLogger.kt
@@ -2,10 +2,16 @@ package com.juul.khronicle
 
 /** Logger for the console, taking advantage of available features such as stderr or js console levels. */
 public expect object ConsoleLogger : Logger {
+
     override fun verbose(tag: String, message: String, metadata: ReadMetadata, throwable: Throwable?)
+
     override fun debug(tag: String, message: String, metadata: ReadMetadata, throwable: Throwable?)
+
     override fun info(tag: String, message: String, metadata: ReadMetadata, throwable: Throwable?)
+
     override fun warn(tag: String, message: String, metadata: ReadMetadata, throwable: Throwable?)
+
     override fun error(tag: String, message: String, metadata: ReadMetadata, throwable: Throwable?)
+
     override fun assert(tag: String, message: String, metadata: ReadMetadata, throwable: Throwable?)
 }
